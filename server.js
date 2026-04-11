d19e45427ce92a3d18610ec962bf58f9fab8fe02
Anchal Gupta <csrl.anchalgupta@gmail.com>
2026-04-11 23:49:17 +0530

    Server.js file updated

--- server.js
+++ server.js
@@ -30,7 +30,5 @@ app.get("/api/protected", authMiddleware, (req, res) => {
   });
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
